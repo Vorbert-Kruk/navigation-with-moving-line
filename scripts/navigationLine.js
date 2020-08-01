741f8cb026afa3859bddcd811e7de0e364b750f2
Vorbert Kruk <pryrokopter@gmail.com>
2020-08-01 11:36:58 +0200

    Added ability to relocate lines

--- scripts/navigationLine.js
+++ scripts/navigationLine.js
@@ -3,17 +3,29 @@ const navLinkClassName = `${navClassName}--link`;
 const navLineClassName = `${navClassName}--line`;
 const activeLinkClassName = `${navLinkClassName}_active`;
 
+const navLine = document.querySelector(`.${navLineClassName}`);
 const navLinks = document.querySelectorAll(`.${navLinkClassName}`);
 const getActiveLink = () => document.querySelector(`.${activeLinkClassName}`);
 
 const setLinkActive = link => link.classList.add(activeLinkClassName);
 const setLinkInactive = link => link.classList.remove(activeLinkClassName);
 
+const getItemDimentions = item => item && item.getBoundingClientRect();
+
 navLinks.forEach(navLink => {
-  navLink.addEventListener('click', () => {
+  navLink.addEventListener('click', event => {
     const currentlyActiveLink = getActiveLink();
     setLinkInactive(currentlyActiveLink);
 
     setLinkActive(navLink);
+    const linkOffset = navLink.offsetLeft;
+    const linkWidth = navLink.scrollWidth;
+
+    relocateLine(linkOffset, linkWidth);
   });
 });
+
+const relocateLine = (leftOffset, width) => {
+  navLine.style.transform = `translateX(${leftOffset}px)`;
+  navLine.style.width = `${width}px`;
+};
